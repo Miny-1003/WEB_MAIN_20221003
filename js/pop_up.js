72558c8b6ad1ec80e2ea009659a695942320be89
Miny <mymy5177@naver.com>
2025-06-07 12:22:57 +0900

    3week_12

--- js/pop_up.js
+++ js/pop_up.js
@@ -77,3 +77,12 @@ function closePopup() {
         self.close();
     }
 }
+
+// 팝업 이벤트
+function over(obj) {
+    obj.src = "../image/LOGO_VAL.png";
+}
+
+function out(obj) {
+    obj.src = "../image/LOGO_RIOT.png";
+}
