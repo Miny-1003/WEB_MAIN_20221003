93cda08fb63aa6343a0835c850d160854b4adbb3
Miny <mymy5177@naver.com>
2025-06-07 15:53:32 +0900

    13week_36

--- js/pop_up.js
+++ js/pop_up.js
@@ -37,3 +37,12 @@ document.addEventListener("DOMContentLoaded", function () {
         pop_up();
     }
 });
+
+// 팝업 이벤트
+function over(obj) {
+    obj.src = "image/LOGO_VAL.png";
+}
+
+function out(obj) {
+    obj.src = "image/LOGO_RIOT.png";
+}
