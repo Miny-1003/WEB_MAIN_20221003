30a8f66c1139fdac93c4cab3b1bd6055d52dbbcf
Miny <mymy5177@naver.com>
2025-06-07 12:43:43 +0900

    3week_17

---
+++ js/pop_up_img.js
@@ -0,0 +1,8 @@
+// 팝업 이벤트
+function over(obj) {
+    obj.src = "../image/LOGO_VAL.png";
+}
+
+function out(obj) {
+    obj.src = "../image/LOGO_RIOT.png";
+}
--- js/search.js
+++ js/search.js
@@ -39,12 +39,3 @@ function googleSearch() {
     window.open(googleSearchUrl, "_blank");
     return false;
 }
-
-// // 팝업 이벤트
-// function over(obj) {
-//     obj.src = "../image/LOGO_VAL.png";
-// }
-
-// function out(obj) {
-//     obj.src = "../image/LOGO_RIOT.png";
-// }
