f733c54597944fb49836e1c67279b5785535b34c
Miny <mymy5177@naver.com>
2025-06-07 11:54:27 +0900

    3week_6

--- js/search.js
+++ js/search.js
@@ -39,11 +39,12 @@ function googleSearch() {
     window.open(googleSearchUrl, "_blank");
     return false;
 }
+
 // 팝업 이벤트
 function over(obj) {
-    obj.src = "../image/LOGO_VAL.png";
+    obj.src = "image/LOGO_VAL.png";
 }
 
 function out(obj) {
-    obj.src = "../image/LOGO_RIOT.png";
+    obj.src = "image/LOGO_RIOT.png";
 }
