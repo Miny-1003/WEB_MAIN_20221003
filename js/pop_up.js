7d8e3daf3b03e8f6d9e32968561bc78529240b91
Miny <mymy5177@naver.com>
2025-06-07 15:28:48 +0900

    13week_31

--- js/pop_up.js
+++ js/pop_up.js
@@ -6,56 +6,76 @@ function setCookie(name, value, expiredays) {
 
 function getCookie(name) {
     var cookie = document.cookie;
-    console.log("쿠키를 요청합니다.");
-    if (cookie != "") {
+    if (cookie !== "") {
         var cookie_array = cookie.split("; ");
-        for ( var index in cookie_array) {
+        for (var index in cookie_array) {
             var cookie_name = cookie_array[index].split("=");
-            if (cookie_name[0] == "popupYN") {
+            if (cookie_name[0] === name) {
                 return cookie_name[1];
             }
         }
     }
-    return ;
+    return;
 }
 
 function closePopup() {
-    if (document.getElementById('check_popup').checked) {
+    const checkbox = document.getElementById('check_popup');
+    if (checkbox && checkbox.checked) {
         setCookie("popupYN", "N", 1);
         console.log("쿠키를 설정합니다.");
-        self.close();
+        window.close();
     }
 }
 
-function pop_up() {
-    var cookieCheck = getCookie("popupYN");
-        if (cookieCheck !== "N"){
-        window.open("popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+// 카운트다운 자동 종료
+let countdown = 300; // 초 단위, 원하는 시간으로 설정 가능
+let countdownTimer;
+
+function showCountdown() {
+    const timeDiv = document.getElementById("Time");
+    if (timeDiv) {
+        timeDiv.innerText = `${countdown}초 후 창이 닫힙니다.`;
+        countdown--;
+        if (countdown >= 0) {
+            countdownTimer = setTimeout(showCountdown, 1000);
+        } else {
+            window.close();
+        }
     }
 }
 
-// 갱신을 수행하는 날짜 함수
-function show_clock(){
-    let currentDate= new Date(); 
-    let divClock= document.getElementById('divClock');
+// 시계 표시 함수
+function show_clock() {
+    let currentDate = new Date();
+    let divClock = document.getElementById('divClock');
     let msg = "현재 시간: ";
-    if(currentDate.getHours()>12){ 
-        msg += "오후"
-        msg += currentDate.getHours()-12 +"시";
+    if (currentDate.getHours() > 12) {
+        msg += "오후 ";
+        msg += currentDate.getHours() - 12 + "시 ";
+    } else {
+        msg += "오전 ";
+        msg += currentDate.getHours() + "시 ";
     }
-    else {
-        msg += "오전"
-        msg += currentDate.getHours()+"시";
-    }
-    msg += currentDate.getMinutes()+"분";
-    msg += currentDate.getSeconds()+"초";
-    divClock.innerText= msg;
+    msg += currentDate.getMinutes() + "분 ";
+    msg += currentDate.getSeconds() + "초";
 
-    if(currentDate.getMinutes()>58){ 
-        divClock.style.color="red";
+    divClock.innerText = msg;
+
+    if (currentDate.getMinutes() > 58) {
+        divClock.style.color = "red";
+    } else {
+        divClock.style.color = "black";
     }
-    setTimeout(show_clock, 1000) 
+
+    setTimeout(show_clock, 1000);
 }
+
+// DOM이 준비되면 시계와 카운트다운 시작
+document.addEventListener("DOMContentLoaded", function () {
+    show_clock();
+    showCountdown();
+});
+
 
 // 팝업 이벤트
 function over(obj) {
@@ -65,7 +85,3 @@ function over(obj) {
 function out(obj) {
     obj.src = "image/LOGO_RIOT.png";
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  pop_up(); // 페이지 로딩 끝나고 자동으로 팝업 실행
-});
